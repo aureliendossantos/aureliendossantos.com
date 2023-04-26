99231be1ac4ce040be5b80cd8e9c3c939ebd576f
Aurélien Dos Santos <31006039+aureliendossantos@users.noreply.github.com>
2023-04-26 02:22:56 +0200

    Make RSS discoverable in head

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -14,18 +14,16 @@ export async function get(context: APIContext) {
 		description: "Contenu récent sur le blog d'Aurélien Dos Santos.",
 		site: context.site + "",
 		customData: `<language>fr-fr</language>`,
-		items: articles
-			.filter((post) => !post.data.draft)
-			.map((post) => ({
-				title: post.data.title,
-				pubDate: post.data.date,
-				description: post.data.description,
-				customData: `<language>fr-fr</language>`,
-				// Compute RSS link from post `slug`
-				// This example assumes all posts are rendered as `/blog/[slug]` routes
-				link: `/blog/${post.slug}/`,
-				// Note: this will not process components or JSX expressions in MDX files.
-				// content: sanitizeHtml(parser.render(post.body)),
-			})),
+		items: articles.map((post) => ({
+			title: post.data.title,
+			pubDate: post.data.date,
+			description: post.data.description,
+			customData: `<language>fr-fr</language>`,
+			// Compute RSS link from post `slug`
+			// This example assumes all posts are rendered as `/blog/[slug]` routes
+			link: `/blog/${post.slug}/`,
+			// Note: this will not process components or JSX expressions in MDX files.
+			// content: sanitizeHtml(parser.render(post.body)),
+		})),
 	})
 }
