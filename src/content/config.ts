ff5f5b71cb11706fd3f670eaa90a34ea9ea43fbe
Aurélien Dos Santos <31006039+aureliendossantos@users.noreply.github.com>
2023-04-27 02:58:17 +0200

    refactor: fusion between Article and Page & between blog and tufte

--- src/content/config.ts
+++ src/content/config.ts
@@ -28,29 +28,14 @@ export const collections = {
 				places: z.array(z.nativeEnum(Places)).default([]),
 			}),
 	}),
-	tufte: defineCollection({
-		schema: ({ image }) =>
-			z.object({
-				title: z.string(),
-				description: z.string().optional(),
-				date: z.date(),
-				categories: z.array(z.string()).default([]),
-				tags: z.array(z.string()).default([]),
-				image: image().optional(),
-				imageAnchorTop: z.boolean().optional(),
-				draft: z.boolean().default(false),
-				palette: z.nativeEnum(Palettes).default(Palettes.default),
-				games: z.array(z.string()).optional(),
-				books: z.array(z.string()).optional(),
-				places: z.array(z.nativeEnum(Places)).default([]),
-			}),
-	}),
 	pages: defineCollection({
 		schema: ({ image }) =>
 			z.object({
 				title: z.string(),
 				description: z.string().optional(),
+				opengraph: image().optional(),
 				image: image().optional(),
+				imageAnchorTop: z.boolean().optional(),
 				cover: z.boolean().default(false),
 				toc: z.boolean().default(false),
 				depth: z.onumber(),
