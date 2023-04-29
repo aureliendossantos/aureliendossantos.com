df77157234e745f69e189a728c1ca33dc6d70f12
Aurélien Dos Santos <31006039+aureliendossantos@users.noreply.github.com>
2023-04-29 05:23:59 +0200

    feat(games): styling

--- src/utils/getIGDBgame.ts
+++ src/utils/getIGDBgame.ts
@@ -1,6 +1,7 @@
 import enumerate from "./enumerateStrings"
 
 export default async function getIGDBgame(slug: string) {
+	console.log("Getting IGDB data: " + slug)
 	const authentication = await fetch(
 		`https://id.twitch.tv/oauth2/token?client_id=${import.meta.env.TWITCH_ID}&client_secret=${
 			import.meta.env.TWITCH_SECRET
@@ -25,10 +26,9 @@ export default async function getIGDBgame(slug: string) {
 	const request = await fetch(
 		`https://api.igdb.com/v4/games`,
 		parameters(
-			`fields name, cover.image_id, screenshots.image_id, platforms.abbreviation, first_release_date, release_dates.y, involved_companies.developer, involved_companies.publisher, involved_companies.company.name, websites.category, websites.url, url; where slug = "${slug}";`
+			`fields name, cover.image_id, cover.width, cover.height, screenshots.image_id, screenshots.width, screenshots.height, platforms.abbreviation, first_release_date, release_dates.y, involved_companies.developer, involved_companies.publisher, involved_companies.company.name, websites.category, websites.url, url; where slug = "${slug}";`
 		)
 	).then((response) => response.json())
-
 	const game = request[0]
 	game.developers = enumerate(
 		game.involved_companies.filter((i: any) => i.developer).map((i: any) => i.company.name)
@@ -36,6 +36,5 @@ export default async function getIGDBgame(slug: string) {
 	game.publishers = enumerate(
 		game.involved_companies.filter((i: any) => i.publisher).map((i: any) => i.company.name)
 	)
-
 	return game
 }
