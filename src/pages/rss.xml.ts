5015053b909cb0b67b50a3d8395457f2af7968b2
Aurélien Dos Santos <31006039+aureliendossantos@users.noreply.github.com>
2023-11-14 16:25:01 +0100

    feat(rss): diary entries with date in feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,27 +3,37 @@ import type { APIContext } from "astro"
 import { getCollection } from "astro:content"
 import sanitizeHtml from "sanitize-html"
 import MarkdownIt from "markdown-it"
-import getBlogPosts from "$utils/getBlogPosts"
+import getBlogPosts, { getDiary } from "$utils/getBlogPosts"
 const parser = new MarkdownIt()
 
-export async function get(context: APIContext) {
-	const articles = await getBlogPosts()
+export async function GET(context: APIContext) {
+	const blog = await getBlogPosts()
+	const diary = (await getDiary()).filter((post) => post.data.date)
 	return rss({
 		stylesheet: "/rss/pretty-feed-v3.xsl",
 		title: "Aurélien Dos Santos",
-		description: "Contenu récent sur le blog d'Aurélien Dos Santos.",
+		description: "Contenu récent sur le site d'Aurélien Dos Santos.",
 		site: context.site + "",
 		customData: `<language>fr-fr</language>`,
-		items: articles.map((post) => ({
-			title: post.data.title,
-			pubDate: post.data.date,
-			description: post.data.description,
-			customData: `<language>fr-fr</language>`,
-			// Compute RSS link from post `slug`
-			// This example assumes all posts are rendered as `/blog/[slug]` routes
-			link: `/blog/${post.slug}/`,
-			// Note: this will not process components or JSX expressions in MDX files.
-			// content: sanitizeHtml(parser.render(post.body)),
-		})),
+		items: [
+			...blog.map((post) => ({
+				title: post.data.title,
+				pubDate: post.data.date,
+				description: post.data.description,
+				customData: `<language>fr-fr</language>`,
+				link: `/blog/${post.slug}/`,
+				// Note: this will not process components or JSX expressions in MDX files.
+				// content: sanitizeHtml(parser.render(post.body)),
+			})),
+			...diary.map((post) => ({
+				title: post.data.title,
+				pubDate: post.data.date as Date,
+				description: "Entrée de journal",
+				customData: `<language>fr-fr</language>`,
+				link: `/diary/${post.finalSlug}/`,
+				// Note: this will not process components or JSX expressions in MDX files.
+				// content: sanitizeHtml(parser.render(post.body)),
+			})),
+		].sort((a, b) => (a.pubDate > b.pubDate ? -1 : 1)),
 	})
 }
