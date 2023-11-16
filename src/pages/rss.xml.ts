e3956292abba0c3b32e46dedf405818485c6e618
Aurélien Dos Santos <31006039+aureliendossantos@users.noreply.github.com>
2023-11-16 23:20:09 +0100

    feat(rss): cover image in content

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -16,17 +16,23 @@ export async function GET(context: APIContext) {
 		site: context.site + "",
 		customData: `<language>fr-fr</language>`,
 		items: [
-			...blog.map((post) => ({
-				title: post.data.title,
-				pubDate: post.data.date,
-				description: post.data.description,
-				customData: `<language>fr-fr</language>`,
-				link: `/blog/${post.slug}/`,
-				// Note: this will not process components or JSX expressions in MDX files.
-				// content: sanitizeHtml(parser.render(post.body)),
-			})),
+			...blog.map((post) => {
+				const image = post.data.opengraph || post.data.image
+				return {
+					title: post.data.title,
+					author: "Aurélien Dos Santos",
+					pubDate: post.data.date,
+					description: post.data.description,
+					customData: `<language>fr-fr</language>`,
+					link: `/blog/${post.slug}/`,
+					content: image ? `<img src="${image.src}"/>` : undefined,
+					// Note: this will not process components or JSX expressions in MDX files.
+					// content: sanitizeHtml(parser.render(post.body)),
+				}
+			}),
 			...diary.map((post) => ({
 				title: post.data.title,
+				author: "Aurélien Dos Santos",
 				pubDate: post.data.date as Date,
 				description: "Entrée de journal",
 				customData: `<language>fr-fr</language>`,
