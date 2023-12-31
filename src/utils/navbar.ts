383130625b9cec1ea0899c8470382d807243fcf2
Aurélien Dos Santos <31006039+aureliendossantos@users.noreply.github.com>
2023-12-31 15:26:19 +0100

    feat(navbar): image zoom adapts to the navbar

--- src/utils/navbar.ts
+++ src/utils/navbar.ts
@@ -1,24 +1,26 @@
 export const toggleNavbar = () => {
 	const navbar = document.querySelector("#navbar") as HTMLDivElement
 	if (navbar.classList.contains("invisible")) {
+		window.mediumZoom.update({
+			container: {
+				height: window.innerHeight - 28,
+			},
+		})
 		navbar.classList.remove("invisible")
 		setTimeout(() => {
 			navbar.classList.remove("-bottom-7")
 			navbar.classList.add("bottom-0")
 		}, 10)
 	} else {
+		window.mediumZoom.update({
+			container: {
+				height: window.innerHeight,
+			},
+		})
 		navbar.classList.add("-bottom-7")
 		navbar.classList.remove("bottom-0")
 		setTimeout(() => {
 			navbar.classList.add("invisible")
-		}, 160)
-	}
-	const homeButton = document.querySelector("#blog-home-navbar-toggle") as HTMLButtonElement
-	// If this button is found, we are on the homepage and we should make the button :active for a short time to animate it
-	if (homeButton) {
-		homeButton.classList.add("active")
-		setTimeout(() => {
-			homeButton.classList.remove("active")
 		}, 160)
 	}
 }
