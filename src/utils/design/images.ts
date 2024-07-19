08d15b0dd879606ac597b5ec0dfe63238f21cfc5
Aurélien Dos Santos <31006039+aureliendossantos@users.noreply.github.com>
2024-07-19 10:31:09 +0200

    feat(blog): dark ArticleCards; header fleuron; misc tweaks

--- src/utils/design/images.ts
+++ src/utils/design/images.ts
@@ -3,7 +3,11 @@ import type { AstroBuiltinAttributes } from "astro"
 export const borderClasses: AstroBuiltinAttributes["class:list"] =
 	"border border-solid border-[--border-color] dark:border-[--dark-border-color]"
 
+export const paperDarkClasses: AstroBuiltinAttributes["class:list"] =
+	"dark:mix-blend-screen dark:hue-rotate-180 dark:invert"
+
 export const paperClasses: AstroBuiltinAttributes["class:list"] = [
 	borderClasses,
-	"mix-blend-multiply dark:mix-blend-screen dark:hue-rotate-180 dark:invert",
+	"mix-blend-multiply",
+	paperDarkClasses,
 ]
